a167b20fd395a45603b2d36cbf96dcb99ccedd60
Jiuyang Liu <liu@jiuyang.me>
2022-10-03 17:22:39 +0800

    fix compile for riscv32.

--- arch/riscv32/atomic_arch.h
+++ arch/riscv32/atomic_arch.h
@@ -24,12 +24,31 @@ static inline int a_sc(volatile int *p, int v)
 static inline int a_cas(volatile int *p, int t, int s)
 {
 	int old, tmp;
-	__asm__("1:  lr.w    %0, %2      \n"
-		"    bne     %0, %3, 1f  \n"
-		"    sc.w    %1, %4, %2  \n"
-		"    bnez    %1, 1b      \n"
-		"1:                      \n"
-		: "=&r"(old), "+r"(tmp), "+A"(*p)
-		: "r"(t), "r"(s));
+	__asm__ __volatile__ (
+		"\n1:	lr.w.aqrl %0, (%2)\n"
+		"	bne %0, %3, 1f\n"
+		"	sc.w.aqrl %1, %4, (%2)\n"
+		"	bnez %1, 1b\n"
+		"1:"
+		: "=&r"(old), "=&r"(tmp)
+		: "r"(p), "r"((long)t), "r"((long)s)
+		: "memory");
+	return old;
+}
+
+#define a_cas_p a_cas_p
+static inline void *a_cas_p(volatile void *p, void *t, void *s)
+{
+	void *old;
+	int tmp;
+	__asm__ __volatile__ (
+		"\n1:	lr.w.aqrl %0, (%2)\n"
+		"	bne %0, %3, 1f\n"
+		"	sc.w.aqrl %1, %4, (%2)\n"
+		"	bnez %1, 1b\n"
+		"1:"
+		: "=&r"(old), "=&r"(tmp)
+		: "r"(p), "r"(t), "r"(s)
+		: "memory");
 	return old;
 }
